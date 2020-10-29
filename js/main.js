e3bb0d3e779ddf40eb9c1d786e7e3370a0b80353
SergeiBoy <s_shchetinin@mail.ru>
2020-10-29 12:24:53 +0500

    Собрали js файлы при помощи gulp-concat

--- js/main.js
+++ js/main.js
@@ -16,3 +16,20 @@
     }
   });
 })();
+
+'use strict';
+
+(function () {
+  try {
+    IMask(
+        document.querySelector('.form__tel input'), {
+          mask: '+{7}(000)000-00-00'
+        })();
+  } catch (e) {
+    if (e.name === 'TypeError') {
+      // Пропускаем ошибку
+    } else {
+      throw e;
+    }
+  }
+})();
