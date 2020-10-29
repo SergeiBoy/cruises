8ab8b7dd28e74db639b814c89a446a7cb3a77a93
SergeiBoy <s_shchetinin@mail.ru>
2020-10-29 23:49:02 +0500

    padding вверху и внизу 13px

--- js/main.js
+++ js/main.js
@@ -18,12 +18,14 @@
 })();
 
 'use strict';
-
+/* global IMask */
+/* eslint no-undef: "error"*/
+/* eslint new-cap: ["error", { "capIsNewExceptions": ["IMask"] }]*/
 (function () {
   try {
     IMask(
         document.querySelector('.form__tel input'), {
-          mask: '+{7}(000)000-00-00'
+          mask: '00000000000'
         })();
   } catch (e) {
     if (e.name === 'TypeError') {
